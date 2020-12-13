969994ab7e8cfdfde63bc9b3c88d57ca409b6b9e
hsoni92 <h.soni1321992@gmail.com>
2020-12-14 05:26:04 +0530

    grid

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Scrollbars } from 'react-custom-scrollbars';
 import StatusCard from './Components/CardLayout/card';
 import Grid from '@material-ui/core/Grid';
 import './App.css';
@@ -12,9 +13,9 @@ class App extends Component {
     const xl = 3;
 
     return (
-      <div>
-        <Grid container justify="center" spacing={4}>
-          <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+      <Scrollbars style={{ height: '100vh', width: '100vw' }}>
+        <Grid container justify="center" spacing={2}>
+          <Grid item xs={10} sm={10} md={10} lg={10} xl={10}>
             <Grid container justify="center" spacing={4}>
               <Grid item xs={xs} sm={sm} md={md} lg={lg} xl={xl}>
                 <StatusCard type="success" percentage={30} title="JS001" />
@@ -28,7 +29,7 @@ class App extends Component {
             </Grid>
           </Grid>
         </Grid>
-      </div>
+      </Scrollbars>
     );
   }
 }
